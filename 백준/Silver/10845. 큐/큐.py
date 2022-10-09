b6d940af184dfc2ee0e82a72f01664a1c5689207
myonjin <109326291+myonjin@users.noreply.github.com>
2022-10-10 02:33:55 +0900

    전체 코드 업로드 -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+# A,B,C = map(int,sys.stdin.readline().split())
+# arr= list(map(int,sys.stdin.readline().split()))
+T=int(input())
+stack=deque()
+for _ in range(T):
+    arr=list(sys.stdin.readline().split())
+    if len(arr)>1:
+        stack.append(arr[1])
+    else:
+        if arr[0]=='front':
+            if stack:
+                print(stack[0])
+            else:
+                print(-1)
+        elif arr[0]=='pop':
+            if stack:
+                s=stack.popleft()
+                print(s)
+            else:
+                print(-1)
+        elif arr[0]=='size':
+            print(len(stack))
+        elif arr[0]=='empty':
+            if stack:
+                print(0)
+            else:
+                print(1)
+        elif arr[0]=='back':
+            if stack:
+                print(stack[-1])
+            else:
+                print(-1)
+
+
+
