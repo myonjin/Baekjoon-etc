60af0458e56c8a889559f3c3e457615a7b940961
myonjin <109326291+myonjin@users.noreply.github.com>
2022-10-23 00:24:59 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1448 ms, Memory: 32952 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -0,0 +1,17 @@
+import sys
+import math
+# from collections import Counter
+# from collections import deque
+# arr=[list(map(int,sys.stdin.readline().rstrip().split())) for _ in range(N)]
+# N,r,c = map(int,sys.stdin.readline().split())
+N=int(input())
+for _ in range(N):
+    A,B = map(int,sys.stdin.readline().split())
+    print(A+B)
+
+
+
+
+
+
+
