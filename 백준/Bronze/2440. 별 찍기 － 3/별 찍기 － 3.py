1e6159781dd780048dbf294c78416077d4552a00
myonjin <109326291+myonjin@users.noreply.github.com>
2022-10-30 04:39:51 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 72 ms, Memory: 32908 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.py
@@ -0,0 +1,11 @@
+import sys
+import heapq
+# from collections import Counter
+# from collections import deque
+# arr=[list(map(int,sys.stdin.readline().rstrip().split())) for _ in range(N)]
+# n = int(input())
+N = int(sys.stdin.readline())
+for i in range(N,-1,-1):
+    print('*'*i)
+
+
