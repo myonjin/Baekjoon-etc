4a405fb9b0e8707ce3902b3eabfcdeb0b5587dcb
myonjin <109326291+myonjin@users.noreply.github.com>
2022-10-30 04:31:15 +0900

    [Silver II] Title: 최대 힙, Time: 188 ms, Memory: 45980 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+# from collections import Counter
+# from collections import deque
+# arr=[list(map(int,sys.stdin.readline().rstrip().split())) for _ in range(N)]
+# n = int(input())
+N = int(sys.stdin.readline())
+hq=[]
+for _ in range(N):
+    M=int(sys.stdin.readline())
+    if M==0:
+        if hq:
+            print(heapq.heappop(hq)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(hq,(-M,M))
+
