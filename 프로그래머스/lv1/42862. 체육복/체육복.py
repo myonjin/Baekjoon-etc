071d366e0b27ea450e8f68c9a23cc4da724e0e70
myonjin <109326291+myonjin@users.noreply.github.com>
2023-05-26 23:53:11 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,17 +1,3 @@
 def solution(n, lost, reserve):
     answer = 0
-    newlost = list(set(lost) - set(reserve))
-    newreserve = list(set(reserve) - set(lost))
-    lost.sort
-    reserve.sort
-    for i in newreserve:
-        for j in newlost:
-            if j == i-1:
-                newlost.remove(j)
-                break
-            elif j == i+1:
-                newlost.remove(j)
-                break
-            
-    answer = n - len(newlost)
     return answer
